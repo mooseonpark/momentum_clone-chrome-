01219edef9200ad380ec612c45464cb8a9505869
mooseonpark <whomsp@gmail.com>
2022-04-08 23:43:46 +0900

    Add: DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+const title = document.getElementById('title');
+console.log(title);
+console.log(title);
+console.log(title);
+console.log(title);
+
+title.innerText = 'changedddd';
+
+console.log(title.id);
+console.log(title.className);
